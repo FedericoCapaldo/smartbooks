70387a3470ec4dc345b7564368e679ef31e1f90f
Federico <fede6993@hotmail.it>
2017-03-09 00:38:03 -0800

    initial stuff:

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+class SearchController < ApplicationController
+
+  def index
+  end
+
+  def search
+    puts 'oooooooooooooooooooooo'
+  end
+
+
+  def result
+    puts "xxxxxxxxxxxxxxxxxxxxx"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'search#index'
+
+  get '/'
+
+  resources :search do
+    member do
+      get '/result'
+    end
+  end
+end
