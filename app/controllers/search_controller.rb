f39fbd44b803076c054ad1ca2b5b3a1485919deb
Federico <fede6993@hotmail.it>
2017-03-09 12:04:21 -0800

    basic search returning json on webapge working.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,23 @@ class SearchController < ApplicationController
   def index
   end
 
-  def search
-    puts 'oooooooooooooooooooooo'
-  end
-
-
   def result
-    puts "xxxxxxxxxxxxxxxxxxxxx"
+    if params[:title] == "" && params[:author] == ""
+      redirect_to '/'
+    else
+      base_url = "http://ec2-52-40-24-42.us-west-2.compute.amazonaws.com:8983/solr/TEXTBOOK_DB/select?indent=on&q="
+      wildcard = "*"
+      title = params[:title].strip.gsub(/\s+/, "\\ ")
+      author = params[:author].strip.gsub(/\s+/, "\\ ")
+      json_format_r = "&wt=json"
+
+      titleQuery = "title:" + wildcard + title + wildcard
+      authorQuery = "author:" + wildcard + author + wildcard
+      final_url = base_url + (title.empty? ? "" : titleQuery ) + ((!title.empty? && !author.empty?) ? " AND " : "") + (author.empty? ? "" : authorQuery) + json_format_r
+
+      uri = URI.parse(final_url)
+      response = Net::HTTP.get(uri)
+      @response = response
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'search#index'
 
-  get '/'
+  get '/' => 'search#index'
 
-  resources :search do
-    member do
-      get '/result'
-    end
-  end
+  get '/result' => 'search#result'
+
 end
