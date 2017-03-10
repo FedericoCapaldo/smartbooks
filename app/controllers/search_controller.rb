53871843d4cb614bafe6ff044c657baf2981531c
Federico <fede6993@hotmail.it>
2017-03-10 12:52:14 -0800

    refactored controller to make request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,22 +7,35 @@ class SearchController < ApplicationController
     if params[:title] == "" && params[:author] == ""
       redirect_to '/'
     else
-      base_url = "http://ec2-52-40-24-42.us-west-2.compute.amazonaws.com:8983/solr/TEXTBOOK_DB/select?indent=on&q="
-      wildcard = "*"
-      title = params[:title].strip.gsub(/\s+/, "\\ ")
-      author = params[:author].strip.gsub(/\s+/, "\\ ")
-      json_format_r = "&wt=json"
 
-      titleQuery = "title:" + wildcard + title + wildcard
-      authorQuery = "author:" + wildcard + author + wildcard
-      final_url = base_url + (title.empty? ? "" : titleQuery ) + ((!title.empty? && !author.empty?) ? " AND " : "") + (author.empty? ? "" : authorQuery) + json_format_r
-
-      uri = URI.parse(final_url)
-      response = Net::HTTP.get(uri)
-
+      final_url = gen_url(params[:title], params[:author])
+      response = make_solr_request(final_url)
       jsonResponse = JSON.parse(response)
 
       @documents = jsonResponse["response"]["docs"]
     end
   end
+  
+  private
+
+  def gen_url(titleParam, authorParam)
+    base_url = "http://ec2-52-40-24-42.us-west-2.compute.amazonaws.com:8983/solr/TEXTBOOK_DB/select?indent=on&q="
+    wildcard = "*"
+    title = titleParam.strip.gsub(/\s+/, "\\ ")
+    author = authorParam.strip.gsub(/\s+/, "\\ ")
+    json_format_r = "&wt=json"
+
+    titleQuery = "title:" + wildcard + title + wildcard
+    authorQuery = "author:" + wildcard + author + wildcard
+    final_url = base_url + (title.empty? ? "" : titleQuery ) + ((!title.empty? && !author.empty?) ? " AND " : "") + (author.empty? ? "" : authorQuery) + json_format_r
+
+    final_url
+  end
+
+  def make_solr_request(url)
+    uri = URI.parse(url)
+    response = Net::HTTP.get(uri)
+    response
+  end
+
 end
