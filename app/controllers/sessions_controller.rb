54f98ef2a0fef29f12a420e8a50123e9be975eff
Federico <fede6993@hotmail.it>
2017-03-12 16:30:07 -0700

    finished acceptable style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       sign_in user
       return_back_or user
     else
-    flash[:error] = "Invalid email/password combination"
+    flash[:danger] = "Invalid email/password combination"
     render 'new'
     end
   end
